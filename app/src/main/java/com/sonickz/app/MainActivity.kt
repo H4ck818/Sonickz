9969019c81904ae63838b594a3d5412ba961b468
H4ck818 <163768138+H4ck818@users.noreply.github.com>
2026-01-12 00:56:33 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/sonickz/app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.sonickz.app
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val text = TextView(this)
+        text.text = "SONICKZ - VPN Core Loading..."
+        text.textSize = 20f
+
+        setContentView(text)
+    }
+}
